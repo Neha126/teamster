bcdcdc0a95e83780a985e161e463d9e4157978c4
Neha Tomar <tneha5161@gmail.com>
2022-09-17 22:10:50 +0530

    firebase setup react-router setup creating signin/signup page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LogIn from "./Components/LogIn/LogIn";
+import Register from "./Components/Register/Register";
+import Reset from "./Components/Reset/Reset";
+import Dashboard from "./Components/Dashboard/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Teamster</h2>
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<LogIn />} />
+          <Route exact path="/Register" element={<Register />} />
+          <Route exact path="/reset" element={<Reset />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,104 @@
+import { initializeApp } from "firebase/app";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDEPUdIT0ZPNWZIJM13Flg2Jfq0-rnbE6g",
+  authDomain: "teamster-8d2d8.firebaseapp.com",
+  projectId: "teamster-8d2d8",
+  storageBucket: "teamster-8d2d8.appspot.com",
+  messagingSenderId: "815001382299",
+  appId: "1:815001382299:web:7d8ca3a3dae4b33c06453e",
+  measurementId: "G-BQ3P9DMK7Q",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const googleProvider = new GoogleAuthProvider();
+const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const user = res.user;
+    const q = query(collection(db, "users"), where("uid", "==", user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const sendPasswordReset = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert("Password reset link sent!");
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+export {
+  auth,
+  db,
+  signInWithGoogle,
+  logInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendPasswordReset,
+  sendPasswordResetEmail,
+  logout,
+};
